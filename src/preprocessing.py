6c8a355fa899dff4bae0c456af8aee1b06ead271
nigorita <neg.rahbar@gmail.com>
2026-04-04 17:46:00 +0200

    add tokenizatin step

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import nltk
+
+nltk.download("punkt")
+nltk.download('punkt_tab')
 
 def load_data():
     df = pd.read_csv("data.csv")
@@ -9,8 +13,15 @@ def lowercase_text(df):
     df["text"] = df["text"].str.lower()
     return df
 
+def tokenize_text(df):
+    df["tokens"] = df["text"].apply(nltk.word_tokenize)
+    return df
+
+
 if __name__ == "__main__":
     df = load_data()
     print(df.head())
     df = lowercase_text(df)
+    print(df.head())
+    df = tokenize_text(df)
     print(df.head())
